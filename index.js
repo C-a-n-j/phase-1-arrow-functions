d9e41f4bd361b06d457ecacaa49ed60e64e84766
C-a-n-j <eddymacharia450@gmail.com>
2024-04-08 10:59:51 +0300

    Complete lab

---
+++ index.js
@@ -0,0 +1,20 @@
+//function divide
+function divide(a, b){
+return a/b;
+}
+divide(2000, 100)
+//an attow function
+ const square = (x) =>{
+    return Math.pow(x, 2)
+
+ }
+ square(2)
+
+ const add=(a, b)=>{
+    return a + b;
+  
+
+ }
+ add(3, 4)
+
+ 
